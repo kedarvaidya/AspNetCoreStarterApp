1c7d4f7502d1801be16175998200301738be73c4
Kedar <kedarvaidya0@gmail.com>
2016-09-10 14:50:13 +0530

    Add Empty Web Project

---
+++ src/StarterApp/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Humanizer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace StarterApp
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("app-settings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"app-settings.{env.EnvironmentName.Underscore().Dasherize()}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            this.Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+			services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+			var supportedCultures = new[] { new CultureInfo("en") };
+			services.Configure<RequestLocalizationOptions>(options =>
+			{
+				options.DefaultRequestCulture = new RequestCulture("en");
+				options.SupportedCultures = supportedCultures;
+				options.SupportedUICultures = supportedCultures;
+				options.FallBackToParentCultures = true;
+				options.FallBackToParentUICultures = true;
+			});
+
+			services.AddMvc()
+				.AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+			app.UseRequestLocalization();
+
+			app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Pages}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ src/StarterApp/TagHelpers/CopyrightTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace StarterApp.TagHelpers
+{
+	public class CopyrightTagHelper : TagHelper
+	{
+		public int FromYear { get; set; } = DateTimeOffset.Now.Year;
+
+		public int ToYear { get; set; } = DateTimeOffset.Now.Year;
+
+		public string Holder { get; set; }
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "span";
+			output.Content.SetHtmlContent($"&copy; {FromYear}{(FromYear != ToYear ? $"-{ToYear}" : "")} {Holder}");
+			output.TagMode = TagMode.StartTagAndEndTag;
+		}
+	}
+}
